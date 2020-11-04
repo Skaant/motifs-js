452f5793176bf0e816fab3523940474bcef17d30
imrok le bleu <romaric.ruga@gmail.com>
2020-11-04 05:54:02 +0100

    moved DOC

---
+++ _motifs/doc/_props/description/description.prop.js
@@ -0,0 +1,6 @@
+export default `General documentation ressources
+
+DOC is composed of few sub-[MOTIFS](#motif) :
+* [README](#readme),
+* [WIKI](#wiki),
+* [WEBSITE](#website).`
--- _shrine/doc/_props/description/description.prop.js
+++
@@ -1,6 +0,0 @@
-export default `**TOP-LEVEL DOCUMENTATION KAMI**
-
-DOC is composed of few sub-KAMIS :
-* [README](#readme),
-* [WIKI](#wiki),
-* [WEBSITE](#website).`
