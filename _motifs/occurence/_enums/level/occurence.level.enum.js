dd9e7be7f239ae5427d1cc9e7be66ab24f18fbef
imrok le bleu <romaric.ruga@gmail.com>
2020-11-15 15:41:31 +0100

    occurence level enum

---
+++ _motifs/occurence/_enums/level/occurence.level.enum.js
@@ -0,0 +1,8 @@
+export default {
+  /** INSTANCES of this MOTIF occur as folder(s). */
+  FOLDER: 'folder',
+  /** INSTANCES of this MOTIF occur as file(s). */
+  FILE: 'file',
+  /**  INSTANCES of this MOTIF occur as property(ies). */
+  PROP: 'prop'
+}
